b438f38140adfa44326755ad73498815a58ff125
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-29 21:43:51 +0000

    added answer.php

---
+++ answer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit5-02PHP-HTML-MDL -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Positive or negative number, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Lynn Lukose" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-pink.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>"Postive or negative #, in PHP"</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Positive or Negative #, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/positive_negative.jpg" alt="postive and negative" />
+      </div>
+      <?php
+      $numberType = $_GET["kind-of-number"];
+      if ($numberType === "1") {
+        $randomNumber = rand(1, 6);
+        echo "Your random number is: " . $randomNumber;
+      } else {
+        $randomNumber = rand(-6, -1);
+        echo "Your random number is: " . $randomNumber;
+      }
+      ?>
+  </div>
+  </main>
+</body>
+
+</html>
